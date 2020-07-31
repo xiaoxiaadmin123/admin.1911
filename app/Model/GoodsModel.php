00f7a5a76492ec1e125d9d09b89f330d37e3c912
xiaoxia <xiaoxia@localhost.localdomain>
2020-07-31 08:24:03 +0000

    初始化仓库

---
+++ app/Model/GoodsModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GoodsModel extends Model
+{
+    //
+    public $table='p_goods';
+    public $primaryKey='goods_id';
+    public $timestamps=false;
+
+
+}
